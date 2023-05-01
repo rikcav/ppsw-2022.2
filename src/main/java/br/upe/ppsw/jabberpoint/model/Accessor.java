cfdcfc925cf6b8d3b8cf0e07ee58c53c6edcafd6
rik <rik@end>
2023-05-01 08:02:03 -0300

    refactor: improving code readability

---
+++ src/main/java/br/upe/ppsw/jabberpoint/model/Accessor.java
@@ -0,0 +1,19 @@
+package br.upe.ppsw.jabberpoint.model;
+
+import java.io.IOException;
+
+public abstract class Accessor {
+
+	public static final String DEMO_NAME = "Apresentação de Demonstração";
+	public static final String DEFAULT_EXTENSION = ".xml";
+
+	public Accessor() {
+	}
+
+	abstract public void loadFile(Presentation presentation, String fileName)
+			throws IOException;
+
+	abstract public void saveFile(Presentation presentation, String fileName)
+			throws IOException;
+
+}
