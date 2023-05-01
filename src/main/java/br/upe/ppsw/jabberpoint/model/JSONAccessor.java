c8dd0a39b03de9a1dbdd3473bda71b280011f332
rik <rik@end>
2023-05-01 08:08:00 -0300

    feat: added class to work with JSON presentations

---
+++ src/main/java/br/upe/ppsw/jabberpoint/model/JSONAccessor.java
@@ -0,0 +1,17 @@
+package br.upe.ppsw.jabberpoint.model;
+
+import java.io.IOException;
+
+public class JSONAccessor extends Accessor {
+
+	@Override
+	public void loadFile(Presentation presentation, String fileName)
+			throws IOException {		
+	}
+
+	@Override
+	public void saveFile(Presentation presentation, String fileName)
+			throws IOException {
+	}
+
+}
