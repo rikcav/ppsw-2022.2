19ad73c68dfd14d8d4eb8facf3135ed61d562df4
rik <rik@end>
2023-05-01 08:06:49 -0300

    refactor: changing model classes from control to model package

--- src/main/java/br/upe/ppsw/jabberpoint/control/IPresentationLoader.java
+++
@@ -1,9 +0,0 @@
-package br.upe.ppsw.jabberpoint.control;
-
-import java.io.FileNotFoundException;
-
-import br.upe.ppsw.jabberpoint.model.Presentation;
-
-public interface IPresentationLoader {
-	public void loadPresentation(Presentation presentation, String unusedFilename) throws FileNotFoundException;
-}
