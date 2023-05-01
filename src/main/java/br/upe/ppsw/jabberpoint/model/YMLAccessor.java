f5ddff0c6a0a18c11bcc527d024ffd178ebaca4a
rik <rik@end>
2023-05-01 16:25:14 -0300

    feat: Created class to work with yaml files

---
+++ src/main/java/br/upe/ppsw/jabberpoint/model/YMLAccessor.java
@@ -0,0 +1,18 @@
+package br.upe.ppsw.jabberpoint.model;
+
+import java.io.IOException;
+
+public class YMLAccessor extends Accessor {
+
+	@Override
+	public void loadFile(Presentation presentation, String fileName)
+			throws IOException {
+
+	}
+
+	@Override
+	public void saveFile(Presentation presentation, String fileName)
+			throws IOException {
+
+	}
+}
